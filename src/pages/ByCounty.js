a1c5a5e6c9d6acc264666c252b612b839e137111
david <dkdavid24@live.com>
2022-05-11 15:47:09 +0300

    tried adding %

--- src/pages/ByCounty.js
+++ src/pages/ByCounty.js
@@ -5,27 +5,33 @@ import { LinePlot } from "d3plus-react";
 import "../App.css";
 
 const ByCounty = () => {
-    const { county } = useParams();
+  const { county } = useParams();
 
-    return (
-      <div className="Chart">
-        <Link to="/">Home</Link>
-        <LinePlot
-          config={{
-            data: `http://localhost:8000/api/county/${county}`,
-            groupBy: "id",
-            baseline : 0,
-          }}
-        />
-        <LinePlot
-          config={{
-            data: `http://localhost:8000/api/county/${county}/pass-rate`,
-            groupBy: "id",
-            baseline : 0,
-          }}
-        />
-      </div>
-    );
+  return (
+    <div className="Chart">
+      <Link to="/">Home</Link>
+      <LinePlot
+        config={{
+          data: `http://localhost:8000/api/county/${county}`,
+          groupBy: "id",
+          baseline: 0,
+        }}
+      />
+      <LinePlot
+        config={{
+          data: `http://localhost:8000/api/county/${county}/pass-rate`,
+          groupBy: "id",
+          baseline: 0,
+          y: "y",
+          yConfing: {
+            tickFormat: function(d) {
+              return `${parseFloat(d) * 100}%`;
+            }
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default ByCounty;
